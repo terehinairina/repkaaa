bd30a0d9b09ff4b002e123fdb07b416cb09559dd
terehinairina <irisha.freyman@mail.ru>
2022-10-03 18:31:51 +0300

    commit

--- src/EggVoice.java
+++ src/EggVoice.java
@@ -6,48 +6,45 @@
         for(int i = 0; i < 5; i++)
         {
             try{
-                sleep(1000); //Приостанавливает поток на 1 секунду
+                sleep(1000);
             }catch(InterruptedException e){}
 
             System.out.println("яйцо!");
         }
-//Слово «яйцо» сказано 5 раз
+
     }
 }
 
-class ChickenVoice //Класс с методом main()
-{
-    static EggVoice mAnotherOpinion; //Побочный поток
+class ChickenVoice {
+    static EggVoice mAnotherOpinion;
 
-    public static void main(String[] args)
-    {
-        mAnotherOpinion = new EggVoice(); //Создание потока
+    public static void main(String[] args) {
+        mAnotherOpinion = new EggVoice();
         System.out.println("Спор начат...");
-        mAnotherOpinion.start(); //Запуск потока
+        mAnotherOpinion.start();
 
-        for(int i = 0; i < 5; i++)
-        {
-            try{
-                Thread.sleep(1000); //Приостанавливает поток на 1 секунду
-            }catch(InterruptedException e){}
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
 
             System.out.println("курица!");
         }
 
 //Слово «курица» сказано 5 раз
 
-        if(mAnotherOpinion.isAlive()) //Если оппонент еще не сказал последнее слово
-        {
-            try{
-                mAnotherOpinion.join(); //Подождать пока оппонент закончит высказываться.
-            }catch(InterruptedException e){}
+        if (mAnotherOpinion.isAlive()) {
+            try {
+                mAnotherOpinion.join();
+            } catch (InterruptedException e) {
+            }
 
             System.out.println("Первым появилось яйцо!");
-        }
-        else //если оппонент уже закончил высказываться
-        {
+        } else
             System.out.println("Первой появилась курица!");
-        }
+
         System.out.println("Спор закончен!");
     }
 }
+
